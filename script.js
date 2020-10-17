100b27d1c6efe215570eb3339447f6a4248a3a9c
Stefan Aikins <stefanaikins@gmail.com>
2020-10-17 19:09:20 +0100

    more structure added to html

---
+++ script.js
@@ -0,0 +1,32 @@
+/* variables to store the questions
+2. questin description
+3. 4 ans for each question
+4. correct or wrong ans
+*/
+
+//click handler for start button
+// click start button everything disappears n question appears
+// show questions
+// show timer (start countdown)
+//view higher scores button
+
+
+// click an answer
+// confirm corect or wrong answer (display a message)
+// deduct time if incorrect
+// score 5 points for correct ans
+// end quiz when all questions are answered 
+// show finish screen, having final score on there.
+
+
+// timer should count down from 120 sec
+// show curent time
+// reduce time by one
+// repeat every second (use set interval)
+// check if is at zero or less and then stop the timer
+
+// type name in fimish screen
+// click a button to save thier score
+//when click the button, store in local storage(check class video for this bit)
+//setItem and getItem group 
+
